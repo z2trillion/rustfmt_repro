41bf0e636f9d289628d2921b897f3bb483f72960
Mason Liang <mason@0x.org>
2020-03-30 17:11:09 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
 ///
-/// * D. H. Bailey (1990). FFTs in external or hierarchical memory. <https://www.davidhbailey.com/dhbpapers/fftq.pdf>
-/// * W. M. Gentleman & G. Sande (1966). Fast Fourier Transforms: for fun and
-///   profit. <https://doi.org/10.1145/1464291.1464352> <http://cis.rit.edu/class/simg716/FFT_Fun_Profit.pdf>
-/// * M. Frigo, C.E. Leiserson, H. Prokop & S. Ramachandran (1999).
-///   Cache-oblivious algorithms. <http://supertech.csail.mit.edu/papers/FrigoLePr99.pdf>
-/// * S. Johnson, M. Frigo (2005). The Design and Implementation of FFTW3. <http://www.fftw.org/fftw-paper-ieee.pdf>
-/// * S. Johnson, M. Frigo (2012). Implementing FFTs in Practice. <https://cnx.org/contents/ulXtQbN7@15/Implementing-FFTs-in-Practice>
-///   <https://www.csd.uwo.ca/~moreno/CS433-CS9624/Resources/Implementing_FFTs_in_Practice.pdf>
+/// - asdfakwefasdfasdf
 
 fn main() {}
